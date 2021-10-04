8d86a7c5d6fee8deeb53c6455c15d46e9f3f71f9
koreaaerospaceU <zx811@naver.com>
2021-10-05 08:24:36 +0900

    feat(error): ErrorStatusCode Enum 클래스 선언 - Error 상태 코드 관리

---
+++ src/main/java/baseball/error/ErrorStatusCode.java
@@ -0,0 +1,12 @@
+package baseball.error;
+
+/**
+ * Created by Minky on 2021-10-05
+ */
+public enum ErrorStatusCode {
+    IS_NOT_BETWEEN_ONE_AND_TWO,
+    IS_NOT_DECIMAL_INPUT,
+    IS_NOT_DECIMAL_ARRAY_ELEMENTS_NOT_EQUAL,
+    IS_NOT_VALID_ARRAY_SIZE,
+    CORRECT
+}
