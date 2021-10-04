c41a32080151be55152e83a458e9e344e95ff2d2
koreaaerospaceU <zx811@naver.com>
2021-10-05 00:38:33 +0900

    feat(random): 숫자야구게임 임의의 수 생성 기능 구현 - [1 - 9] 까지의 임의의 수 3개 생성

--- src/main/java/baseball/domain/BaseballModel.java
+++ src/main/java/baseball/domain/BaseballModel.java
@@ -1,21 +1,32 @@
 package baseball.domain;
 
+import nextstep.utils.Randoms;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 /**
  * Created by Minky on 2021-10-04
  */
 public class BaseballModel {
-    private List<Integer> randomValue;
+    private List<Integer> strikeList;
 
-    public BaseballModel(List<Integer> randomValue) {
-        this.randomValue = randomValue;
+    public BaseballModel() {
+        initStrikeList();
+    }
+
+    public void initStrikeList() {
+        HashSet<Integer> strikeSet = new HashSet();
+        while (strikeSet.size() != 3) {
+            strikeSet.add(Randoms.pickNumberInRange(1, 9));
+        }
+        this.strikeList = new ArrayList(strikeSet);
     }
 
     public int getStrikeCount(int[] userInputArray) {
         int result = 0;
         for (int i = 0; i < 3; i++) {
-            result += compareInt(randomValue.get(i), userInputArray[i]);
+            result += compareInt(strikeList.get(i), userInputArray[i]);
         }
         return result;
     }
