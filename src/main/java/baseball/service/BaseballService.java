9b39fd6a23011d7ec8bf2f9fe03f0831fd14ed3c
koreaaerospaceU <zx811@naver.com>
2021-10-05 09:13:09 +0900

    feat(service): 숫자야구게임 관리 기능 제작 - 게임 변수 초기화 - 사용자 입력 값 관리 - 게임 턴 결과 반환 - 게임 진행 여부 반환 - 게임 통과 여부 반환

--- src/main/java/baseball/service/BaseballService.java
+++ src/main/java/baseball/service/BaseballService.java
@@ -1,11 +1,64 @@
 package baseball.service;
 
-import nextstep.utils.Console;
-
-import java.util.regex.Pattern;
+import baseball.domain.BaseballCounter;
+import baseball.domain.StrikeModel;
+import baseball.domain.BaseballStatusCode;
 
 /**
  * Created by Minky on 2021-10-04
  */
 public class BaseballService {
+    private StrikeModel strikeModel;
+    private int[] userInputArray;
+    private int userInput;
+
+    public BaseballService() {
+        this.strikeModel = new StrikeModel();
+    }
+
+    public void initGame() {
+        strikeModel.initStrikeList();
+    }
+
+    public void setUserInputArray(int[] userInputArray) {
+        this.userInputArray = userInputArray;
+    }
+
+    public void setUserInput(int userInput) {
+        this.userInput = userInput;
+    }
+
+    public BaseballCounter getBaseballCounter() {
+        int strikeCount = strikeModel.getStrikeCount(userInputArray);
+        int ballCount = strikeModel.getBallCount(userInputArray);
+        BaseballStatusCode baseballStatusCode = selectBaseballStatusCode(strikeCount, ballCount);
+        return new BaseballCounter(strikeCount, ballCount, baseballStatusCode);
+    }
+
+    public Boolean isKeepGoing() {
+        if (strikeModel.getStrikeCount(userInputArray) == 3) {
+            return false;
+        }
+        return true;
+    }
+
+    public Boolean isRestart() {
+        if (userInput == 1) {
+            return true;
+        }
+        return false;
+    }
+
+    private BaseballStatusCode selectBaseballStatusCode(int strikeCount, int ballCount) {
+        if (strikeCount == 3) {
+            return BaseballStatusCode.MATCH;
+        } else if (strikeCount > 0 && ballCount > 0) {
+            return BaseballStatusCode.STRIKE_AND_BALL;
+        } else if (strikeCount > 0 && ballCount == 0) {
+            return BaseballStatusCode.STRIKE;
+        } else if (strikeCount == 0 && ballCount > 0) {
+            return BaseballStatusCode.BALL;
+        }
+        return BaseballStatusCode.NOTHING;
+    }
 }
