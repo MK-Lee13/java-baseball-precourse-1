0e31b6868b68ea6f699d7ba7fa8c13b3ba4b4d72
koreaaerospaceU <zx811@naver.com>
2021-10-06 14:06:26 +0900

    test(input, strike): 테스트 DisplayName 지정 - DisplayName 지정 - 스트라이크 0 갯수 검증 테스트 코드 추가 - 볼 0 갯수 검증 테스트 코드 추가

--- src/test/java/baseball/domain/StrikeModelTest.java
+++ src/test/java/baseball/domain/StrikeModelTest.java
@@ -2,6 +2,7 @@ package baseball.domain;
 
 import nextstep.utils.Randoms;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -26,6 +27,7 @@ public class StrikeModelTest {
         setMockStrikeList();
     }
 
+    @DisplayName(value = "3자리 숫자 랜덤값 생성 테스트")
     @Test
     void initStrikeList() {
         try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
@@ -37,7 +39,7 @@ public class StrikeModelTest {
         }
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "3자리 숫자 등록 및 반환 검증 테스트 [{index}] => `{argumentsWithNames}`")
     @CsvSource(value = {"1, 2, 3", "4, 5, 6", "7, 8, 9"})
     void getStrikeList(int targetOne, int targetTwo, int targetThree) {
         List<Integer> mockStrikeList = getMockStrikeList(targetOne, targetTwo, targetThree);
@@ -45,36 +47,56 @@ public class StrikeModelTest {
         assertEquals(mockStrikeList, strikeModel.getStrikeList());
     }
 
+    @DisplayName(value = "스트라이크 갯수 0 반환 테스트")
+    @Test
+    void getStrikeCountReturnZero() {
+        int[] mockUserInputArray = {4, 5, 6};
+        assertEquals(0, strikeModel.getStrikeCount(mockUserInputArray));
+    }
+
+    @DisplayName(value = "스트라이크 갯수 1 반환 테스트")
     @Test
     void getStrikeCountReturnOne() {
         int[] mockUserInputArray = {3, 2, 4};
         assertEquals(1, strikeModel.getStrikeCount(mockUserInputArray));
     }
 
+    @DisplayName(value = "스트라이크 갯수 2 반환 테스트")
     @Test
     void getStrikeCountReturnTwo() {
         int[] mockUserInputArray = {1, 2, 4};
         assertEquals(2, strikeModel.getStrikeCount(mockUserInputArray));
     }
 
+    @DisplayName(value = "스트라이크 갯수 3 반환 테스트")
     @Test
     void getStrikeCountReturnThree() {
         int[] mockUserInputArray = {1, 2, 3};
         assertEquals(3, strikeModel.getStrikeCount(mockUserInputArray));
     }
 
+    @DisplayName(value = "볼 갯수 0 반환 테스트")
+    @Test
+    void getBallCountReturnZero() {
+        int[] mockUserInputArray = {4, 5, 6};
+        assertEquals(0, strikeModel.getBallCount(mockUserInputArray));
+    }
+
+    @DisplayName(value = "볼 갯수 1 반환 테스트")
     @Test
     void getBallCountReturnOne() {
         int[] mockUserInputArray = {4, 5, 2};
         assertEquals(1, strikeModel.getBallCount(mockUserInputArray));
     }
 
+    @DisplayName(value = "볼 갯수 2 반환 테스트")
     @Test
     void getBallCountReturnTwo() {
         int[] mockUserInputArray = {3, 2, 1};
         assertEquals(2, strikeModel.getBallCount(mockUserInputArray));
     }
 
+    @DisplayName(value = "볼 갯수 3 반환 테스트")
     @Test
     void getBallCountReturnThree() {
         int[] mockUserInputArray = {3, 1, 2};
--- src/test/java/baseball/utils/InputTest.java
+++ src/test/java/baseball/utils/InputTest.java
@@ -2,6 +2,7 @@ package baseball.utils;
 
 import baseball.error.ErrorStatusCode;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -26,12 +27,7 @@ public class InputTest {
         input = new Input();
     }
 
-    @Test
-    void getGameFinishUserInput() {
-        setMockUserInputString("0");
-        assertEquals(0, input.getGameFinishUserInput());
-    }
-
+    @DisplayName(value = "숫자야구게임 입력 값 반환 테스트")
     @Test
     void getGameStartUserInput() {
         setMockUserInputString("123");
@@ -39,63 +35,70 @@ public class InputTest {
         assertArrayEquals(expectValue, input.getGameStartUserInput());
     }
 
-    @ParameterizedTest
+    @DisplayName(value = "숫자야구게임 재시작 입력 값 반환 테스트")
+    @Test
+    void getGameFinishUserInput() {
+        setMockUserInputString("0");
+        assertEquals(0, input.getGameFinishUserInput());
+    }
+
+    @ParameterizedTest(name = "숫자야구게임 재시작 정상 값 입력 검증 테스트 [{index}] => `{argumentsWithNames}`")
     @ValueSource(strings = {"1", "2"})
     void validateGameFinishUserInputStatusCorrect(String userInputString) {
         input.setUserInputString(userInputString);
         assertEquals(ErrorStatusCode.CORRECT, input.validateGameFinishUserInput());
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "숫자야구게임 재시작 1, 2 이외의 값 입력 검증 테스트 [{index}] => `{argumentsWithNames}`")
     @ValueSource(strings = {"3", "4", "5", "6", "7", "8", "9"})
     void validateGameFinishUserInputStatusNotBetweenOneAndTwo(String userInputString) {
         input.setUserInputString(userInputString);
         assertEquals(ErrorStatusCode.IS_NOT_BETWEEN_ONE_AND_TWO, input.validateGameFinishUserInput());
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "숫자야구게임 재시작 비정상 값 입력 검증 테스트 [{index}] => `{argumentsWithNames}`")
     @ValueSource(strings = {"-1", "asd", " ", "", "0"})
     void validateGameFinishUserInputStatusNotDecimalInput(String userInputString) {
         input.setUserInputString(userInputString);
         assertEquals(ErrorStatusCode.IS_NOT_DECIMAL_INPUT, input.validateGameFinishUserInput());
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "숫자야구게임 재시작 Null 값 입력 검증 테스트 [{index}] => `{argumentsWithNames}`")
     @NullAndEmptySource
     void validateGameFinishUserInputStatusNotDecimalInputAboutNull(String userInputString) {
         input.setUserInputString(userInputString);
         assertEquals(ErrorStatusCode.IS_NOT_DECIMAL_INPUT, input.validateGameFinishUserInput());
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "숫자야구게임 정상 값 입력 검증 테스트 [{index}] => `{argumentsWithNames}`")
     @ValueSource(strings = {"123", "132", "241", "912", "123"})
     void validateGameStartUserInputStatusCorrect(String userInputString) {
         input.setUserInputString(userInputString);
         assertEquals(ErrorStatusCode.CORRECT, input.validateGameStartUserInput());
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "숫자야구게임 중복 값 입력 검증 테스트 [{index}] => `{argumentsWithNames}`")
     @ValueSource(strings = {"111", "121", "131", "919", "232"})
     void validateGameStartUserInputStatusElementsEqual(String userInputString) {
         input.setUserInputString(userInputString);
         assertEquals(ErrorStatusCode.IS_NOT_DECIMAL_ARRAY_ELEMENTS_NOT_EQUAL, input.validateGameStartUserInput());
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "숫자야구게임 길이 3초과 3미만의 양수 값 입력 검증 테스트 [{index}] => `{argumentsWithNames}`")
     @ValueSource(strings = {"1111", "1122", "241123", "912444", "12", "1"})
     void validateGameStartUserInputStatusNotValidArraySize(String userInputString) {
         input.setUserInputString(userInputString);
         assertEquals(ErrorStatusCode.IS_NOT_VALID_ARRAY_SIZE, input.validateGameStartUserInput());
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "숫자야구게임 비정상 값 입력 검증 테스트 [{index}] => `{argumentsWithNames}`")
     @ValueSource(strings = {"0", "-1", "a", "as", "", " "})
     void validateGameStartUserInputStatusNotDecimalInput(String userInputString) {
         input.setUserInputString(userInputString);
         assertEquals(ErrorStatusCode.IS_NOT_DECIMAL_INPUT, input.validateGameStartUserInput());
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "숫자야구게임 Null 값 입력 검증 테스트 [{index}] => `{argumentsWithNames}`")
     @NullAndEmptySource
     void validateGameStartUserInputStatusNotDecimalInputAboutNull(String userInputString) {
         input.setUserInputString(userInputString);
