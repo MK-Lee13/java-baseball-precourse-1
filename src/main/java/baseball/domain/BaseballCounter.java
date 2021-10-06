36f0941dfb3f61c1e32b39006296c7899b954c06
koreaaerospaceU <zx811@naver.com>
2021-10-06 16:37:19 +0900

    feat(strike): BaseballCounter 객체 생성 시 힌트 메시지 생성 로직 추가

--- src/main/java/baseball/domain/BaseballCounter.java
+++ src/main/java/baseball/domain/BaseballCounter.java
@@ -1,17 +1,21 @@
 package baseball.domain;
 
+import static baseball.message.HintMessage.*;
+
 /**
  * Created by Minky on 2021-10-05
  */
 public class BaseballCounter {
     private int strikeCount;
     private int ballCount;
+    private String baseballMessage;
     private BaseballStatusCode baseballStatusCode;
 
     public BaseballCounter(int strikeCount, int ballCount, BaseballStatusCode baseballStatusCode) {
         this.strikeCount = strikeCount;
         this.ballCount = ballCount;
         this.baseballStatusCode = baseballStatusCode;
+        this.baseballMessage = makeBaseballMessage();
     }
 
     public int getBallCount() {
@@ -22,7 +26,24 @@ public class BaseballCounter {
         return strikeCount;
     }
 
+    public String getBaseballMessage() {
+        return baseballMessage;
+    }
+
     public BaseballStatusCode getBaseballStatusCode() {
         return baseballStatusCode;
+    }
+
+    private String makeBaseballMessage() {
+        if (baseballStatusCode == BaseballStatusCode.STRIKE_AND_BALL) {
+            return String.format(STRIKE_AND_BALL_MESSAGE, strikeCount, ballCount);
+        }
+        if (baseballStatusCode == BaseballStatusCode.STRIKE) {
+            return String.format(STRIKE_MESSAGE, strikeCount);
+        }
+        if (baseballStatusCode == BaseballStatusCode.BALL) {
+            return String.format(BALL_MESSAGE, ballCount);
+        }
+        return NOTHING_MESSAGE;
     }
 }
